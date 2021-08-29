0e07713693b58bd2c7a1ef49ab07aa1e69154d3f
Yago-Caetano <46303870+Yago-Caetano@users.noreply.github.com>
2021-08-29 14:01:07 -0300

    Criação Item Controller

---
+++ src/main/java/com/crud/controllers/ItemController.java
@@ -0,0 +1,29 @@
+package com.crud.controllers;
+
+import com.crud.models.ItemModel;
+
+import java.util.ArrayList;
+
+public class ItemController {
+
+
+    public void inserirItem(ItemModel Item)
+    {
+
+    }
+
+    public void editarItem(ItemModel Item)
+    {
+
+    }
+
+    public void deletarItem(ItemModel Item)
+    {
+
+    }
+
+    public ArrayList<ItemModel> getItem()
+    {
+        return null;
+    }
+}
