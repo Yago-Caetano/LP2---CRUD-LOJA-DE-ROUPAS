f4375ca55039443742225a29e243918618a8a823
Yago-Caetano <46303870+Yago-Caetano@users.noreply.github.com>
2021-08-30 21:57:00 -0300

    Preparação para desenvolvimento dos controllers

--- src/main/java/com/crud/controllers/ItemController.java
+++ src/main/java/com/crud/controllers/ItemController.java
@@ -6,22 +6,37 @@ import java.util.ArrayList;
 
 public class ItemController {
 
-
+    /**
+     *
+     * @param Item
+     */
     public void inserirItem(ItemModel Item)
     {
 
     }
 
+    /**
+     *
+     * @param Item
+     */
     public void editarItem(ItemModel Item)
     {
 
     }
 
+    /**
+     *
+     * @param Item
+     */
     public void deletarItem(ItemModel Item)
     {
 
     }
 
+    /**
+     *
+     * @return
+     */
     public ArrayList<ItemModel> getItem()
     {
         return null;
--- src/main/java/com/crud/dao/ItemDAO.java
+++ src/main/java/com/crud/dao/ItemDAO.java
@@ -6,19 +6,8 @@ import java.sql.DriverManager;
 
 public class ItemDAO {
 
-    public ItemDAO() {  //DESKTOP-H7JQKH4\\SQLEXPRESS; Database=N2_dudu_Viotti_ecommerce; integrated security=true
-        String connectionUrl = "jdbc:sqlserver:DESKTOP-H7JQKH4\\\\SQLEXPRESS;databaseName=master;user=sa;password=123456";
-        /*"jdbc:sqlserver://localhost:1433;databaseName=master;user=sa;password=123456";*/
+    public ItemDAO() {
 
-        try {
-            // Load SQL Server JDBC driver and establish connection.
-            System.out.print("Connecting to SQL Server ... ");
-            try (Connection connection = DriverManager.getConnection(connectionUrl)) {
-                System.out.println("Done.");
-            }
-        } catch (Exception e) {
-            System.out.println();
-            e.printStackTrace();
-        }
+
     }
 }
